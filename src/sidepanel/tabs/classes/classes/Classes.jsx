29f8e2f4c8585a9ba531c130f9fe399bc41bdc2c
Unknowncata <renwatanabemc@gmail.com>
2025-11-23 19:49:24 +0900

    fixed sorted classes

--- src/sidepanel/tabs/classes/classes/Classes.jsx
+++ src/sidepanel/tabs/classes/classes/Classes.jsx
@@ -2,24 +2,35 @@ import { useState } from "react";
 import ClassList from "./ClassList.jsx";
 import CollectedSyllabuses from "../collectedSyllabuses/CollectedSyllabuses.jsx";
 
+const week = ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"];
+
 function Classes({ classes }) {
-  const [selectedClass, setSelectedClass] = useState({});
+    const [selectedClass, setSelectedClass] = useState({});
 
-  if (!classes || Object.keys(classes).length === 0) {
-    return <h1>CoursePowerの講義一覧にアクセスしてください</h1>;
-  }
+    if (!classes || Object.keys(classes).length === 0) {
+        return <h1>CoursePowerの講義一覧にアクセスしてください</h1>;
+    }
 
-  if (Object.keys(selectedClass).length !== 0) {
-    return <CollectedSyllabuses selectedClass={selectedClass} setSelectedClass={setSelectedClass} />;
-  }
+    if (Object.keys(selectedClass).length !== 0) {
+        return <CollectedSyllabuses selectedClass={selectedClass} setSelectedClass={setSelectedClass} />;
+    }
 
-  return (
-    <ul className="flex flex-col gap-5">
-      {Object.entries(classes).map(([dayLabel, classItems], i) => (
-        <ClassList dayLabel={dayLabel} classItems={classItems} setSelectedClass={setSelectedClass} key={i} />
-      ))}
-    </ul>
-  );
+    return (
+        <ul className="flex flex-col gap-5">
+            {Object.keys(classes)
+                .sort((a, b) => {
+                    return week.indexOf(a) - week.indexOf(b);
+                })
+                .map((dayLabel, i) => (
+                    <ClassList
+                        dayLabel={dayLabel}
+                        classItems={classes[dayLabel]}
+                        setSelectedClass={setSelectedClass}
+                        key={i}
+                    />
+                ))}
+        </ul>
+    );
 }
 
 export default Classes;
--- todos.md
+++ todos.md
@@ -1,12 +1,6 @@
 features that i want to add in the future
 
 \***\*\*\*\*\*\*** Neccessary \***\*\*\*\*\*\***
-sort the classes by day
-
-design {
-make each classes information more readable
-add loading screen
-}
 
 \***\*\*\*\*\*\*** Maybes \***\*\*\*\*\*\***
 prob fix the prop drillings
